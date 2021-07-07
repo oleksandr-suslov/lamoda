a3b01d9fcf16e7831d8b2915b03e2bc8f494cf9f
Oleksandr Suslov <Oleksandr.Suslov@gmail.com>
2021-07-07 17:18:48 +0300

    day 2

--- index.js
+++ index.js
@@ -32,6 +32,32 @@ const enableScroll = () => {
     // document.body.style.overflow = '';
 }
 
+//get base goods
+
+const getData = async () => {
+    const data = await fetch('db.json');
+
+    if (data.ok) {
+        return data.json();
+    } else { 
+        throw new Error(`Данные не были полученны, ошибка ${data.status} ${data.statusText}`)
+    };
+};
+
+const getGoods = (callback, value) => {
+        getData()
+        .then(data => {
+            if (value) {
+                callback(data.filter(item => item.category === value))
+            } else {
+                callback(data)
+            }
+        })
+        .catch(err => {console.error(err)
+        })
+};
+// getGoods((data) => {console.warn(data)});
+
 //modal window
 
 const subheaderCart = document.querySelector('.subheader__cart')
@@ -53,3 +79,56 @@ cartOverlay.addEventListener('click', event => {
         cartModalClose();   
     }
 })
+
+// render cards goods
+let hash = location.hash.substring(1);
+console.log(location);
+try {
+    const goodsList = document.querySelector('.goods__list');
+    if (!goodsList) {
+        throw 'This is not a goods page!'
+    }
+    const createCard = ({ id, preview, coast, brand, name, sizes}) => {
+        const li = document.createElement('li');
+        li.classList.add('goods__item');
+        li.innerHTML = `
+        <article class="good">
+                            <a class="good__link-img" href="card-good.html#${id}">
+                                <img class="good__img" src="goods-image/${preview}" alt="">
+                            </a>
+                            <div class="good__description">
+                                <p class="good__price">${coast} &#8381;</p>
+                                <h3 class="good__title">${brand} <span class="good__title__grey">/ ${name}</span></h3>
+                                ${sizes ?
+                                    `<p class="good__sizes">Размеры (RUS): <span class="good__sizes-list">${sizes.join(' ')}</span></p>` :
+                                    ''}
+                                <a class="good__link" href="card-good.html#${id}">Подробнее</a>
+                            </div>
+                        </article>
+        `
+        return li;
+    }
+
+    const renderGoodsList = data => {
+        goodsList.textContent = '';
+
+        data.forEach(item => {
+            const card = createCard(item);
+            goodsList.append(card)
+        })
+    }
+
+    window.addEventListener('hashchange', () => {
+        hash = location.hash.substring(1);
+        getGoods(renderGoodsList, hash);
+        
+        const goodsTitle = document.querySelector('.goods__title');
+
+        goodsTitle.textContent = hash;
+
+    })
+getGoods(renderGoodsList, hash);
+
+} catch (err) {
+    console.warn(err)
+}
